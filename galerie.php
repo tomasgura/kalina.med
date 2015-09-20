ceaadd5b17d54e557776e7df78cbb916bb1bcb9a
Tomáš Gůra <tomas.gura@economia.cz>
2015-09-20 18:45:15 +0200

    cista stranka galerie

---
+++ galerie.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+		<?php require_once './incl/header.php'; ?>
+		<?php require_once './incl/fnc.php'; ?>
+		
+        <title>Jindřích Kalina - Pražský med</title>
+    </head>
+    <body>
+		<div class="content">
+		<?php require_once './incl/pageHeader.php'; ?>
+		<?php menu('galerie') ?>	
+			<div class="row">
+				<div class="vlevo col-md-6">
+
+				</div>
+			</div>
+		</div>
+		<?php require_once './incl/pageFooter.php'; ?>
+
+    </body>
+</html>
