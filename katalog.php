c79d19e302dcb8ce78b52b2372ee8ecace250ede
Tomáš Gůra <tomas.gura@economia.cz>
2015-10-08 18:21:24 +0200

    stranka katalog a o nas

---
+++ katalog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+		<?php require_once './incl/header.php'; ?>
+		<?php require_once './incl/fnc.php'; ?>
+		
+        <title>Katalog - Jindřích Kalina - Pražský med</title>
+    </head>
+    <body>
+		<div class="content">
+		<?php require_once './incl/pageHeader.php'; ?>
+		<?php menu('katalog') ?>	
+			<div class="row">
+				<div class="vlevo col-md-6">
+					<h2>Katalog</h2>
+					<p>Stránka se připravuje...</p>
+				</div>
+				<img class="vpravo" src="img/vcela.jpg">
+			</div>
+		</div>
+		<?php require_once './incl/pageFooter.php'; ?>
+
+    </body>
+</html>
---
+++ o_nas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+		<?php require_once './incl/header.php'; ?>
+		<?php require_once './incl/fnc.php'; ?>
+		
+        <title>O nás - Jindřích Kalina - Pražský med</title>
+    </head>
+    <body>
+		<div class="content">
+		<?php require_once './incl/pageHeader.php'; ?>
+		<?php menu('o_nas') ?>	
+			<div class="row">
+				<div class="vlevo col-md-6">
+					<h2>O nás</h2>
+					<p>Stránka se připravuje</p>
+
+				</div>
+				<img class="vpravo" src="img/vcela.jpg">
+			</div>
+		</div>
+		<?php require_once './incl/pageFooter.php'; ?>
+
+    </body>
+</html>
